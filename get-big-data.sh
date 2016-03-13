198721427513736043f44b87254741658be2774b
Piotr Szul <piotr.szul@gmail.com>
2016-03-14 10:56:25 +1100

    Updated

--- get-big-data.sh
+++ get-big-data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 set -e
-
-wget https://archive.ics.uci.edu/ml/machine-learning-databases/00203/YearPredictionMSD.txt.zip -O big-data/YearPredictionMSD.txt.zip
+mkdir -p big-data
+wget http://archive.ics.uci.edu/ml/machine-learning-databases/00203/YearPredictionMSD.txt.zip -O big-data/YearPredictionMSD.txt.zip

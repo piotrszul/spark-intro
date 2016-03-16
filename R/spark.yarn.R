ae4c6ec7c9fea525bddec97d01f6668fd982d8aa
Piotr Szul <piotr.szul@gmail.com>
2016-03-16 17:12:07 +1100

    Added R cluster examples

--- R/spark.yarn.R
+++
@@ -1,3 +0,0 @@
-library(SparkR,lib.loc=file.path(Sys.getenv("SPARK_HOME"),"R","lib"))
-sc <- sparkR.init(master="yarn-client", sparkPackages=c("com.databricks:spark-csv_2.10:1.4.0"))
-dc <- sparkRSQL.init(sc)
